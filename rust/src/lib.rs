2cc9094f7d7a432a93282fe108507786873e15d0
destinyfvcker <destinyfunk@163.com>
2024-09-09 14:17:20 +0800

    test dependencies

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod player;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use godot::prelude::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
